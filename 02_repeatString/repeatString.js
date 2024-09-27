0d5dcda433482d640aba7bf6684a261615a7399c
Rhomer <rhomersiegfred.agustin254591@gmail.com>
2024-09-27 22:01:00 +0800

    finished the repeating strings

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(line, number) {
+    if(number<0)
+        return "ERROR";
+    
+    let fullLine = "";
+    for(let i = 0; i<number; i++){
+        fullLine += line;
+    }
+    return fullLine;
 };
 
 // Do not edit below this line
