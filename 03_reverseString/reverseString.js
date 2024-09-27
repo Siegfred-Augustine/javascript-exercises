87d921de49c93192b39453637849f8fdc84d2bad
Rhomer <rhomersiegfred.agustin254591@gmail.com>
2024-09-27 22:11:12 +0800

    finsihed the 3rd one

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(line) {
+    let newLine = "";
+    for(let i = 1; i<=line.length; i++){
+        newLine += line[line.length - i];
+    }
+    return newLine;
 };
 
 // Do not edit below this line
