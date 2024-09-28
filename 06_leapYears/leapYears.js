b626cd93bafdd7bc28dba1eb69d6ff7618225959
Rhomer <rhomersiegfred.agustin254591@gmail.com>
2024-09-28 12:20:40 +0800

    completed 6 and 7

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,20 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    function isDivisible(year, divisor){
+        if(year%divisor === 0)
+            return true;
+        return false;
+    }
 
+    if(isDivisible(year, 100)){
+        if(isDivisible(year, 400))
+            return true;
+        return false;
+    }
+    if(isDivisible(year, 4)){
+        return true;
+    } 
+    
+    return false;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+function roundOff(value){
+  return Math.round(value * 10) / 10;
+}
+const convertToCelsius = function(value) {
+  return roundOff(((value - 32) * (5/9)));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(value) {
+  return roundOff((value * (9/5)) + 32);
 };
 
 // Do not edit below this line
